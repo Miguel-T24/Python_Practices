daac227af57fe1ba56022690fbe2ad9dbd7b0b0d
Miguel-T24 <miguelt2296@gmail.com>
2023-03-11 23:05:45 -0500

    Add from 76 to 80 python practice

---
+++ Basics-Exercises_I/78. find_available.py
@@ -0,0 +1,7 @@
+# 78. Write a python program to find the available built-in modules
+
+import sys
+import textwrap
+
+module_name = ", ".join(sorted(sys.builtin_module_names))
+print(textwrap.fill(module_name, width = 70))
---
+++ Basics-Exercises_I/80. get_current_value.py
@@ -0,0 +1,5 @@
+# 80. Write a python program to get current value of the recursion limit
+
+import sys
+print("Current value of the recursion limit:")
+print(sys.getrecursionlimit())
