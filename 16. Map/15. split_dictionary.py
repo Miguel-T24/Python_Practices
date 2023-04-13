a602c516d4f3460dfd9ac1e0756dd0f02e8d41ec
Miguel-T24 <miguelt2296@gmail.com>
2023-04-13 10:53:37 -0500

    Add form 11 to 15 python map pracitce

---
+++ 16. Map/15. split_dictionary.py
@@ -0,0 +1,10 @@
+# 15. Write a python program to split a given dictiornary of lists into list of dictionaries using the map function
+
+def list_of_dicts(marks):
+    result = map(dict, zip(*[[(key, val) for val in value] for key, value in marks.items()]))
+    return list(result)
+marks = {'Science': [88, 89, 62, 95], 'Language': [77, 78, 84, 80]}
+print("Original dictionary of lists:")
+print(marks)
+print("\nSplit said dictionary of lists into list of dictionaries:")
+print(list_of_dicts(marks))
