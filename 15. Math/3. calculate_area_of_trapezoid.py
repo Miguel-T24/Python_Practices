7e31315aeb94c9ed133b688e116a7fe647caef65
Miguel-T24 <miguelt2296@gmail.com>
2023-04-24 17:43:18 -0500

    delete math practice

--- 15. Math/3. calculate_area_of_trapezoid.py
+++
@@ -1,9 +0,0 @@
-# 3. Write a python program to calculate the area of a trapezoid
-
-height = float(input("Hieght of trapezoid: "))
-base_1 = float(input("Base one values: "))
-base_2 = float(input("base two values: "))
-
-area = ((base_1 + base_2) / 2 ) * height
-
-print("Area is : {}".format(area))
