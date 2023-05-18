afb30ea7b67ce9fcd6aca6638a29d5b2a5bbd55d
Miguel-T24 <miguelt2296@gmail.com>
2023-05-18 13:01:08 -0500

    add notes to namedtuple concept

--- collections_and_Itertools/Collections/1. namedtuple.py
+++ collections_and_Itertools/Collections/1. namedtuple.py
@@ -20,48 +20,55 @@ print("Jane Age: ",jane[1])
 for i in [bob,jane]:
     print("Name: {} - Age: {} - Gender: {}".format(*i))
 
+# Name: bob - Age: 30 - Gender: Male
+# Name: Jane - Age: 29 - Gender: Female
+
 print("\n"+"*"*40+"\n")
+# ****************************************
 
 import collections
-Person = collections.namedtuple("Person", 'name age')
+Person = collections.namedtuple("Person", 'name age') # creating tuple with name
 
 bob = Person(name = "Bob", age = 30)
 print("Representation bob: {}".format(bob))
+# Representation bob: Person(name='bob',age=30)
 
-jane = Person(name = 'jane', age = 29)
+jane = Person(name = 'jane', age = 29) # Representation of jane
 print("Jane age: {}".format(jane.age))
+# Jane age: 29
 
 for i in [bob,jane]:
     print("Name: {} - Age: {}".format(*i))
+# Name: bob - Age: 30
+# Name: jane - Age: 29
 
 # Invalid names
 # Person = collections.namedtuple("Person", "name class age") # Error because class is not valid
 
-# We can mada valid names
+# We can't put invalid names
 Person = collections.namedtuple("Person", "name class age" , rename = True)
 Person1 = collections.namedtuple("Person1","name age age", rename = True)
 print(Person._fields) # _1 class
 print(Person1._fields)# _2 dulicates field
 
 # Special attribute
-
 Person = collections.namedtuple("Person",'name age')
 bob = Person(name = "bob", age = 30)
-print("Representation: {}".format(bob))
-print("Fields: {}".format(bob._fields))
+print("Representation: {}".format(bob)) # Represetation: Person(name="bob",age=30)
+print("Fields: {}".format(bob._fields)) # Fields: ('name','age')
 
 # We can convert to instance OrderDict using _asdict()
 Person = collections.namedtuple("Person", 'name age')
 
 bob = Person(name = 'bob', age = 30)
-print("Representation: {}".format(bob))
-print("As Dictionary: {}".format(bob._asdict()))
+print("Representation: {}".format(bob)) # Person(name='bob', age='30')
+print("As Dictionary: {}".format(bob._asdict())) # As Dictionary: {'name':'bob','age':30}
 
 
 # Replace()
 Person = collections.namedtuple("Person" , "name age")
 bob = Person(name = "Bob", age = 30)
-print("Before: {}".format(bob))
+print("Before: {}".format(bob)) # Person('name':'bob', age=30)
 bob2 = bob._replace(name = "Robert")
-print("After: {}".format(bob2))
+print("After: {}".format(bob2)) # Person(name:'Robert',age=30)
 print("Same?? : ", bob is bob2) # False
