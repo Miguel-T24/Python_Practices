3e237e3b3ee1b27e0d906111cda782e01e70ad80
Miguel-T24 <miguelt2296@gmail.com>
2023-05-19 01:54:11 -0500

    add orderdict and defaultdict notes

--- collections_and_Itertools/Collections/5. OrderedDict.py
+++ collections_and_Itertools/Collections/5. OrderedDict.py
@@ -10,8 +10,12 @@ d['a'] = 'A'
 d['b'] = 'B'
 d['c'] = 'C'
 
+print(d) # {'a': 'A', 'b': 'B', 'c': 'C'}
 for k,v in d.items():
     print("{} : {}".format(k,v))
+# a : A
+# b : B
+# c : C
     
 print("\nOrdered Dict")
 d = collections.OrderedDict()
@@ -19,13 +23,20 @@ d['a'] = 'A'
 d['b'] = 'B'
 d['c'] = 'C'
 
+print(d) # OrderedDict([('a', 'A'), ('b', 'B'), ('c', 'C')])
 for k,v in d.items():
     print("{} : {}".format(k,v))
-
+# a : A
+# b : B
+# c : C
 
 print("\nCompare Ordered Dict")
 
-print('dict:', end=' ')
+import collections
+
+print("\nCompare Ordered Dict")
+
+print('Normal dict:', end=' ')
 d1 = {}
 d1['a'] = 'A'
 d1['b'] = 'B'
@@ -36,9 +47,11 @@ d2['c'] = 'C'
 d2['b'] = 'B'
 d2['a'] = 'A'
 
-print(d1 == d2)
+print(d1 == d2) # True
+print(d1) # {'a': 'A', 'b': 'B', 'c': 'C'}
+print(d2) # {'c': 'C', 'b': 'B', 'a': 'A'}
 
-print('OrderedDict:', end=' ')
+print('\nOrderedDict:', end=' ')
 
 d1 = collections.OrderedDict()
 d1['a'] = 'A'
@@ -50,7 +63,10 @@ d2['c'] = 'C'
 d2['b'] = 'B'
 d2['a'] = 'A'
 
-print(d1 == d2)
+print(d1 == d2) # False
+print(d1) # OrderedDict([('a', 'A'), ('b', 'B'), ('c', 'C')])
+print(d2) # OrderedDict([('c', 'C'), ('b', 'B'), ('a', 'A')])
+print("False because the order is important")
 
 print("\nReorder")
 
@@ -61,15 +77,42 @@ d = collections.OrderedDict(
 print('Before:')
 for k, v in d.items():
     print(k, v)
+"""
+Before:
+a A
+b B
+c C
+"""
 
 d.move_to_end('b')
 
 print('\nmove_to_end():')
 for k, v in d.items():
     print(k, v)
+"""
+move_to_end():
+a A
+c C
+b B
+"""
 
 d.move_to_end('b', last=False)
 
 print('\nmove_to_end(last=False):')
 for k, v in d.items():
     print(k, v)
+"""
+b B
+a A
+c C
+"""
+
+print("\npop()")
+d.pop('c')
+for k,v in d.items():
+    print("{}: {}".format(k,v))
+"""
+pop()
+a: A
+b: B
+"""
--- collections_and_Itertools/Collections/6. defaultDict.py
+++ collections_and_Itertools/Collections/6. defaultDict.py
@@ -8,6 +8,6 @@ def default_factory():
     return 'default value'
 
 d = collections.defaultdict(default_factory, foo='bar')
-print('d:', d)
-print('foo =>', d['foo'])
-print('bar =>', d['bar'])
+print('d:', d) # d: defaultdict(<function default_factory at 0x7f6e410d7b00>, {'foo': 'bar'})
+print('foo =>', d['foo']) # bar
+print('bar =>', d['bar']) # default value
